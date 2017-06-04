a6eabacff2a548c1bf2e85664aeff41079348320
Pavel <pavelgolubev345@gmail.com>
2017-06-05 00:44:26 +0300

    upload api

--- server.go
+++ server.go
@@ -2,22 +2,105 @@ package main
 
 import (
 	"encoding/json"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
+
+const MAX_FILE_SIZE int = 8388608 // 268435456 TODO
 
 type Ping struct {
 	Text string `json:"text"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+type Error struct {
+	Reason string `json:"reason"`
+}
+
+type Upload struct {
+	Filename string `json:"filename"`
+	Mimetype string `json:"mimetype"`
+}
+
+func request_failed(w http.ResponseWriter, reason string) {
+	error_ := &Error{
+		Reason: reason,
+	}
+	json.NewEncoder(w).Encode(error_)
+}
+
+func wrong_url_handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	log.Print(r)
+
+	request_failed(w, "Page do not exist")
+}
+
+func ping_handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	log.Print(r)
+
 	ping := &Ping{
 		Text: "IU4 Ping",
 	}
-	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(ping)
 }
 
+func upload_handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	log.Print(r)
+
+	if r.Method != "POST" {
+		request_failed(w, "Wrong method")
+		return
+	}
+
+	if len, err := strconv.Atoi(r.Header.Get("Content-Length")); err != nil || len > MAX_FILE_SIZE {
+		log.Print(len, err)
+		request_failed(w, "File too big")
+		return
+	}
+
+	if err := r.ParseMultipartForm(32 << 20); err != nil {
+		log.Print(err)
+		request_failed(w, "Upload failed")
+		return
+	}
+
+	file, handler, err := r.FormFile("payload")
+	if err != nil {
+		log.Print(err)
+		request_failed(w, "Upload failed")
+		return
+	}
+	defer file.Close()
+
+	f, err := os.OpenFile("./db/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		log.Print(err)
+		request_failed(w, "Upload failed")
+		return
+	}
+	defer f.Close()
+
+	if _, err := io.Copy(f, file); err != nil {
+		log.Print(err)
+		request_failed(w, "Upload failed")
+		return
+	}
+	resp := &Upload{
+		Filename: handler.Filename,
+		Mimetype: handler.Header.Get("Content-Type"),
+	}
+	json.NewEncoder(w).Encode(resp)
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+	os.Mkdir("db", 0777)
+	http.HandleFunc("/", wrong_url_handler)
+	http.HandleFunc("/ping", ping_handler)
+	http.HandleFunc("/upload", upload_handler)
 	http.ListenAndServe(":8080", nil)
 }
--- test/test.js
+++ test/test.js
@@ -1,17 +1,98 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var should = chai.should();
+var fs = require('fs');
 
 chai.use(chaiHttp);
 
 describe('RestApi', function() {
-	describe('/', function() {
-		it('should ping', function(done) {
+	describe('/wrong_url', function() {
+		it('page do not exist', function(done) {
 			chai.request('http://127.0.0.1:8080')
-			.get('/')
+			.get('/wrong_url')
+			.end(function(err, res) {
+				res.should.have.status(200);
+				res.body.should.have.property('reason', 'Page do not exist');
+				done();
+			});
+		});
+	});
+
+	describe('/ping', function() {
+		it('ping with get', function(done) {
+			chai.request('http://127.0.0.1:8080')
+			.get('/ping')
 			.end(function(err, res) {
 				res.should.have.status(200);
 				res.body.should.have.property('text', 'IU4 Ping');
+				done();
+			});
+		});
+
+		it('ping with post', function(done) {
+			chai.request('http://127.0.0.1:8080')
+			.post('/ping')
+			.end(function(err, res) {
+				res.should.have.status(200);
+				res.body.should.have.property('text', 'IU4 Ping');
+				done();
+			});
+		});
+	});
+
+	describe('/upload', function() {
+		it('upload jpg file', function(done) {
+			chai.request('http://127.0.0.1:8080')
+			.post('/upload')
+			.attach('payload', 'test/murat.jpg')
+			.end(function(err, res) {
+				res.should.have.status(200);
+				res.body.should.have.property('filename', 'murat.jpg');
+				res.body.should.have.property('mimetype', 'image/jpeg');
+				done();
+			});
+		});
+
+		it('upload pdf file', function(done) {
+			chai.request('http://127.0.0.1:8080')
+			.post('/upload')
+			.attach('payload', 'test/okp.pdf')
+			.end(function(err, res) {
+				res.should.have.status(200);
+				res.body.should.have.property('filename', 'okp.pdf');
+				res.body.should.have.property('mimetype', 'application/pdf');
+				done();
+			});
+		});
+
+		it('upload not with POST', function(done) {
+			chai.request('http://127.0.0.1:8080')
+			.get('/upload')
+			.end(function(err, res) {
+				res.should.have.status(200);
+				res.body.should.have.property('reason', 'Wrong method');
+				done();
+			});
+		});
+
+		it('upload file with invalid field name', function(done) {
+			chai.request('http://127.0.0.1:8080')
+			.post('/upload')
+			.attach('invalid_filed_name', 'test/okp.pdf')
+			.end(function(err, res) {
+				res.should.have.status(200);
+				res.body.should.have.property('reason', 'Upload failed');
+				done();
+			});
+		});
+
+		it('upload too big file', function(done) {
+			chai.request('http://127.0.0.1:8080')
+			.post('/upload')
+			.attach('payload', 'test/large.exe')
+			.end(function(err, res) {
+				res.should.have.status(200);
+				res.body.should.have.property('reason', 'File too big');
 				done();
 			});
 		});
